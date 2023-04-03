9d83dcd2266eac7e0c4da6db81dcf95e2264d2f3
Dishu12341234 <divyasnhundley@gmail.com>
2023-04-03 12:34:52 +0530

    new file:   add 	new file:   add.c 	new file:   main 	new file:   main.c

---
+++ add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    printf("Enter a number\n");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("%0.2i\n\a",sum);
+    return 0;
+}
+
+//%d -> decimal
+//%i -> integer
